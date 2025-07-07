6ca79ab607861683d4b2e910e34dbaa5e32152fa
Code Monkey King <freesoft.for.people@gmail.com>
2025-07-07 16:45:52 +0300

    looks stable if not forcing resign

--- playok.js
+++ playok.js
@@ -6,17 +6,15 @@ const KATAGO_PATH = '/home/cmk/katago/katago';
 const KATAGO_NET = '/home/cmk/katago/kata1-b10c128.txt.gz';
 const KATAGO_CONFIG = '/home/cmk/katago/gtp.cfg';
 
-function init() {
-  katago = spawn(KATAGO_PATH, ['gtp', '-model', KATAGO_NET, '-config', KATAGO_CONFIG]);
-  setTimeout(function () { socket = connect(); }, 5000);
-  side = 0;
-  katagoSide = -1;
-  TABLE = 0;
-  game = 0;
-} init();
+katago = spawn(KATAGO_PATH, ['gtp', '-model', KATAGO_NET, '-config', KATAGO_CONFIG]);
+setTimeout(function () { socket = connect(); }, 5000);
+side = 0;
+katagoSide = -1;
+TABLE = 0;
+joinedTable = 0;
+activeGame = 0;
 
 function acceptChallenge(socket, color, player, table) {
-  console.log('playok: attempting to play ' + color + ' vs ', player);
   message(socket, 'join', table);
   message(socket, color, table);
   message(socket, 'start', table);
@@ -27,39 +25,48 @@ function message(socket, action, table) {
   switch (action) {
     case 'join':
       request.i = [72, table];
+      TABLE = table;
+      joinedTable = 1;
+      katagoSide = -1;
+      console.log('playok: joined table #' + table);
       break;
     case 'leave':
-      console.log('playok: leaving table');
+      console.log('playok: leaving table #' + table);
       katago.stdin.write('clear_board\n');
       request.i = [73, table];
-      TABLE = 0;
-      game = 0;
+      side = 0;
       katagoSide = -1;
+      TABLE = 0;
+      joinedTable = 0;
+      activeGame = 0;
       break;
     case 'white':
       request.i = [83, table, 1];
       katagoSide = 1;
+      console.log('playok: took white stones at table #' + table);
       break;
     case 'black':
       request.i = [83, table, 0];
       katagoSide = 0;
+      console.log('playok: took black stones at table #' + table);
       break;
     case 'start':
       request.i = [85, table];
-      TABLE = table;
-      game = 1;
-      console.log('playok: joined table at https://www.playok.com/en/go/#' + table);
+      console.log('playok: attempting to start a game at table #' + table);
       setTimeout(function() {
-        if (katagoSide == 0) {
-          katago.stdin.write('clear_board\n');
-          katago.stdin.write('genmove B\n');
-          katago.stdin.write('showboard\n');
+        if (!activeGame) {
+          console.log('playok: opponent refused to start game at table #' + table);
+          message(socket, 'leave', table);
+        } else if (activeGame) {
+          if (katagoSide == 0) {
+            katago.stdin.write('genmove B\n');
+            katago.stdin.write('showboard\n');
+          }
         }
-      }, 12000);
+      }, 2000);
       break;
     case 'resign':
       request.i = [93, table, 4, 0];
-      game = 0;
       break;
     case 'pass':
       request.i = [92, table, 0, 400, 0];
@@ -104,14 +111,9 @@ function connect() {
       let table = response.i[1];
       let player1 = response.s[1];
       let player2 = response.s[2];
-      if ((player1 == 'cgn811g' && player2 == '') || (player2 == 'cgn811g' && player1 == '')) {
-        console.log('playok: opponent left');
-        message(socket, 'leave', table);
-        return;
-      }
-      if (game == 1) return;
+      if (joinedTable == 1) return;
       // DEBUG
-      //if (player2 != 'cmk') return;
+      //if (player1 != 'cmk') return;
       if (response.i[3] == 1 && response.i[4] == 0) {
         acceptChallenge(socket, 'white', player1, table);
       }
@@ -120,7 +122,7 @@ function connect() {
       }
     }
     
-    if (response.i[0] == 91 && response.i[1] == TABLE && game) { // load game
+    if (response.i[0] == 91 && response.i[1] == TABLE && joinedTable) { // load game
       katago.stdin.write('clear_board\n');
       side = 0;
       let moves = response.s;
@@ -129,13 +131,14 @@ function connect() {
           let color = side ? 'W' : 'B';
           if (move == '-') katago.stdin.write('play ' + color + ' pass\n');
           else katago.stdin.write('play ' + color + ' ' + move.replace('-', '').split(' ')[0] + '\n');
+          side ^= 1;
         }
-        katago.stdin.write('showboard\n');
-        console.log('katago: loaded game moves');
       }
+      katago.stdin.write('showboard\n');
+      console.log('playok: loaded game at table #' + TABLE);
     }
   
-    if (response.i[0] == 92 && response.i[1] == TABLE && game) { // update move
+    if (response.i[0] == 92 && response.i[1] == TABLE && joinedTable) { // update move
       let color = '';
       let move = response.s[0];
       if (move != undefined) {
@@ -153,75 +156,86 @@ function connect() {
         side ^= 1;
         if (side == katagoSide) {
           color = katagoSide ? 'W' : 'B';
-          setTimeout(function() {
-            katago.stdin.write('genmove ' + color + '\n');
-            katago.stdin.write('showboard\n');
-          }, 2000);
+          katago.stdin.write('genmove ' + color + '\n');
+          katago.stdin.write('showboard\n');
         }
       }
     }
     
     if (response.i[0] == 81 && response.i[1] == TABLE) { // chat messages & system notifications
-      if (response.s[0].includes('does not agree')) message(socket, 'resign', response.i[1]);
+      console.log('playok:', response.s[0]);
+      //if (response.s[0].includes('does not agree')) message(socket, 'resign', response.i[1]);
       if (response.s[0].includes('resigns') ||
           response.s[0].includes('territory') ||
           response.s[0].includes('exceeded') ||
           response.s[0].includes('booted') ||
           response.s[0].includes('offline') ||
-          response.s[0].includes('table')) {
+          response.s[0].includes('displaced')) {
             message(socket, 'leave', response.i[1]);
           }
     }
-    
+
+    if (response.i[0] == 90) {
+      if (response.i[3] == -1) activeGame = 0;
+      else activeGame = 1;
+    }
+
     if (response.i[0] == 90 && response.i[2] == 53) {
-      if (game) {
+      if (joinedTable) {
         message(socket, 'pass', response.i[1]);
-        console.log('katago: done');
+        console.log('playok: counting game');
       }
     }
+    
+    // DEBUG
+    //if (response.i[1] == TABLE) console.log('playok:', response);
   });
   socket.on('error', function (error) { console.log('playok: error'); });
   socket.on('close', function () {
-    console.log('playok: connection closed');
     katago.kill();
+    console.log('\n\nkatago: killed');
+    console.log('playok: connection closed');
     process.exit();
   }); return socket;
 }
 
 katago.stdout.on('data', (data) => {
   let response = data.toString();
-  if (response.length >= 6 && response.length <= 10) {
-    if (response.includes('pass')) {
-      console.log('katago: PASS');
-      message(socket, 'pass', TABLE);
-    } else if (response.includes('resign')) {
-      console.log('katago: RESIGN');
-      message(socket, 'resign', TABLE);
-    } else {
-      let move = response.split('= ')[1].trim();
-      let col = 'ABCDEFGHJKLMNOPQRST'.indexOf(move[0]);
-      let row = 19-parseInt(move.slice(1));
-      let sq = row * 19 + col;
+  const isMove = response.match(/= ([A-T][0-9]+)\n\n/);
+  if (isMove) {
+    let move = isMove[1];
+    let col = 'ABCDEFGHJKLMNOPQRST'.indexOf(move[0]);
+    let row = 19-parseInt(move.slice(1));
+    let sq = row * 19 + col;
+    setTimeout(function() {
       console.log('katago: generated move', move);
       let message = JSON.stringify({"i": [92, TABLE, 0, (row * 19 + col), 0]});
       socket.send(message);
-    }
+    }, 1000);
+  } else if (response.includes('pass')) {
+    console.log('katago: PASS');
+    message(socket, 'pass', TABLE);
+  } else if (response.includes('resign')) {
+    console.log('katago: RESIGN');
+    message(socket, 'resign', TABLE);
+  } else if (response.includes('A B C D E F')) {
+    // DEBUG
+    //console.log('katago:', '#' + TABLE, response);
   } else {
     if (data.toString().includes('illegal')) {
-      console.log('playok: something went wrong')
+      console.log('katago: something went wrong')
     }
-    
-    // DEBUG
-    console.log('katago:', data.toString());
+    //DEBUG
+    //console.log('katago(DEBUG):', data.toString());
   }
 });
 
 katago.stderr.on('data', (data) => {
-  console.log('katago:', data.toString());
+  console.log('katago(err):', data.toString());
 });
 
-process.on('SIGINT', function() {
-  socket.close();
-  katago.kill();
-  console.log('\n\nkatago: killed');
+process.on('SIGINT', function() { // Ctrl-C: force resign
+  if (side == katagoSide) message(socket, 'resign', TABLE);
 });
+
+// Ctrl-\ to quit
